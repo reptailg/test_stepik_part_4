9323f51887289d11796464fbc9267f9048ec31c0
Dmitriy Perepelitsa <dmitriy.perepelitsa@sibit.tech>
2021-06-08 17:41:55 +0700

    fixed same error

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,4 +1,5 @@
-from .pages.base_page import BasePage
+from pages.base_page import BasePage
+
 from selenium.webdriver.common.by import By
 
 
--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,10 @@
-from pages.main_page import MainPage
+import pages.main_page
+
+
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page = pages.main_page.MainPage(browser,
+                                    link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
