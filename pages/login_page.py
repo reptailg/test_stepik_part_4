343341a3b47c9ce85f1b25d65bf07dbe8db83dda
Dmitriy Perepelitsa <dmitriy.perepelitsa@sibit.tech>
2021-06-09 11:43:18 +0700

    added a new test for login page

---
+++ pages/login_page.py
@@ -0,0 +1,25 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+
+class LoginPage(BasePage):
+
+
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        #print(self.browser.current_url)
+        assert "login" in self.browser.current_url, "URL is wrong!"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM),"Login form NOT FOUND!"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form NOT FOUND!"
--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,17 @@
 import pages.main_page
+import pages.login_page
 
+link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
     page = pages.main_page.MainPage(browser,
                                     link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
     page.should_be_login_link()
+    page.go_to_login_page()
+    page2 = pages.login_page.LoginPage(browser,browser.current_url)
+    page2.should_be_login_url()
+    page2.should_be_login_form()
+    page2.should_be_register_form()
+
